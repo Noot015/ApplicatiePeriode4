e61a4c129457b7c7790cde7a1a23a563df895fc6
Noot015 <0957904@hr.nl>
2018-06-02 19:18:27 +0200

    alleOudeCode
    alle oude code

---
+++ App1/App1/Gebruiker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    class Gebruiker
+    {
+        public string idCode;
+        public string voornaam { get; protected set; }
+        public string tussenvoegsel { get; protected set; }
+        public string achternaam { get; protected set; }
+        public string email { get; protected set; }
+        public string profielfoto; 
+        private string wachtwoord;
+        public DateTime datumLid;
+    }
+}
