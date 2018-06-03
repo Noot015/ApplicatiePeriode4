fc90566037b6531fb60c567fbd6863c7fa9ee6dc
Raritoken <0956540@hr.nl>
2018-06-03 16:37:44 +0200

    Added HomePage & AddOffer

---
+++ App1/App1/AddOffer.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddOffer : ContentPage
+	{
+		public AddOffer ()
+		{
+			InitializeComponent ();
+		}
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new HomePage();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
